692f3f0a4505dde43cc51a4d6d1e6cce33ae1f67
Valentine Dorum <38982055+Pomidorum1989@users.noreply.github.com>
2018-05-04 16:52:11 +0300

    Update Dorum_task_2.js

--- Dorum_task_2.js
+++ Dorum_task_2.js
@@ -1,7 +1,7 @@
-var startday = new Date();
-a = startday.setHours(0,0,0);
-console.log(a);
-var now = (new Date()).getTime();
-console.log(now);
-let result = (now - a)/1000;
-console.log (result);
+function time(result) {
+    startday = new Date();
+    a = startday.setHours(0,0,0);
+    now = (new Date()).getTime();
+    return result = (now - a)/1000;   
+}
+console.log(time(result))
